9724b848e5abb03be60bdded03966cec6423399a
Til-Weimann <67155539+Til-Weimann@users.noreply.github.com>
2022-04-05 15:37:38 +0200

    Update 5533Convert.py
    Use LE instead of BE

--- 5533Convert.py
+++ 5533Convert.py
@@ -17,8 +17,8 @@ for filename in os.listdir(path):
     if (filename.endswith(input_type)):
         print('Converting ' + filename + '...')
 
-        # Read raw 16-bit file, skipping BOM with offset=2. Reshape.
-        raw = np.fromfile(path + '\\' + filename, dtype=np.dtype('>u2'), offset=bom).reshape((h,w))
+        # Read raw 16-bit file, skipping BOM with offset=BOM. Reshape.
+        raw = np.fromfile(path + '\\' + filename, dtype=np.dtype('<u2'), offset=bom).reshape((h,w))
 
         # RGBA5533 packed into uint16
         R = (np.bitwise_and(raw, 0xf800) >> 8).astype(np.uint8)
@@ -29,12 +29,12 @@ for filename in os.listdir(path):
         # Stack the individual channels into RGBA
         RGBA = np.dstack((R,G,B,A))
 
-        # Display or save
+        # Save
         Image.fromarray(RGBA).save(path + "\\" + filename[:-len(input_type)] + ".png")
 
 
 # Alternate way of doing this via ImageMagick:
-# FOR /R %a IN (*.buffer) DO magick -size 512x512+0 -endian MSB -depth 16 gray:%~dpna.buffer -write MPR:orig +delete ^
+# FOR /R %a IN (*.buffer) DO magick -size 512x512+0 -endian LSB -depth 16 gray:%~dpna.buffer -write MPR:orig +delete ^
 #   ( MPR:orig -evaluate and 63488 ) ^
 #   ( MPR:orig -evaluate and 1984  -evaluate leftshift 5  ) ^
 #   ( MPR:orig -evaluate and 56    -evaluate leftshift 10 ) ^
