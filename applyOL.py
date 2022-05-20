31d5ec415ffaa95591711e6bd7b893e87c09d7c2
Til-Weimann <67155539+Til-Weimann@users.noreply.github.com>
2022-05-20 17:19:31 +0200

    Update applyOL.py

--- applyOL.py
+++ applyOL.py
@@ -4,10 +4,10 @@ from PIL import Image
 
 Image.MAX_IMAGE_PIXELS = None
 
-olMask = np.array(Image.open(os.path.join(os.getcwd(), "olM.png"))).astype('bool')
+olMask = np.array(Image.open(os.path.join(os.getcwd(), "olMask.png"))).astype('bool')
 bg = np.array(Image.open(os.path.join(os.getcwd(), "bg.png"))).astype(np.uint8)
 ol = np.array(Image.open(os.path.join(os.getcwd(), "ol.png"))).astype(np.uint8)
 
 out = Image.fromarray(bg * np.invert(olMask) + ol * olMask)
 
-out.save(os.path.join(os.getcwd(), "merged.png"))
+out.save(os.path.join(os.getcwd(), "result.png"))
