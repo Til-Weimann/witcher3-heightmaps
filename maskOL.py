dbc2818f459abf7a4b0cff3892835d573717f781
Til-Weimann <67155539+Til-Weimann@users.noreply.github.com>
2022-05-20 11:48:51 +0200

    Add files via upload

---
+++ maskOL.py
@@ -0,0 +1,14 @@
+import os
+import numpy as np
+from PIL import Image
+
+Image.MAX_IMAGE_PIXELS = None
+
+sm = np.array(Image.open(os.path.join(os.getcwd, "sm.png")))
+sc = np.array(Image.open(os.path.join(os.getcwd, "sc.png"))) * 8192 + 8192
+
+olM = sm < sc
+
+out = Image.fromarray(olM.astype('bool'))
+
+out.save(os.path.join(os.getcwd, "olM.png"))
