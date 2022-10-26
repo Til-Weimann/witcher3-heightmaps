10d191252b07cde8ca4e3dea3256fa8aa638e057
Til-Weimann <67155539+Til-Weimann@users.noreply.github.com>
2022-10-26 20:16:15 +0200

    Update fplot.py

--- fplot.py
+++ fplot.py
@@ -1,16 +1,21 @@
 from PIL import Image
-import json
 import numpy as np
 import os
 
 path = os.getcwd()
-base = 8192
-prec = 2
+res = 8192
 
 for f in os.listdir(path):
     if f.endswith(".txt"):
-        map = np.zeros((base * prec, base * prec))
+        map = np.zeros((res, res))
+        posX = 0
+        posY = 0
         with open(os.path.join(path, f)) as data:
-            for i in json.load(data):
-                map[round((i["PosX"] + base/2) * prec), round((i["PosY"] + base/2) * prec)] = 1
-            Image.fromarray(np.rot90(map).astype(bool)).save(os.path.join(path, f[:-4] + ".png"))
+            for line in data.readlines():
+                if "Yaw(Float):" in line and -res/2 < posX < res/2 and -res/2 < posY < res/2:
+                    map[round(posX + res/2), round(posY + res/2)] = 1
+                if "PositionX(Float):" in line:
+                    posX = float(line.replace("PositionX(Float):","").replace("\n","").replace(" ","").replace(",","."))
+                if "PositionY(Float):" in line:
+                    posY = float(line.replace("PositionY(Float):","").replace("\n","").replace(" ","").replace(",","."))
+        Image.fromarray(np.rot90(map).astype(bool)).save(os.path.join(path, f[:-4] + ".png"))
